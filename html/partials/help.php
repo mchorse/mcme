d176b449fd6fc5070d2f16fb4110cc21317b899a
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-09-28 12:13:02 +0100

    Adjusted help modal styles

--- html/partials/help.php
+++ html/partials/help.php
@@ -405,13 +405,14 @@
             button, export the texture, and use generated texture as a 
             reference for for created model.
         </p>
+        
+        <h3>Additional information</h3>
+        
+        <p>
+            Hosted on <a href="https://github.com/mchorse/mcme" target="_blank">GitHub</a>. 
+            My <a href="https://www.youtube.com/channel/UCWVDjAcecHHa8UrEWMRGI8w" target="_blank">channel</a>. 
+            Tested by <a href="https://www.youtube.com/user/Heilerable" target="_blank">NlL5</a>. 
+            Updates on <a href="http://twitter.com/McHorsy" target="_blank">Twitter</a>.
+        </p>
     </div>
-</div>
-
-<div class="mc-credits">
-    Hosted on <a href="https://github.com/mchorse/mcme" target="_blank">GitHub</a>. 
-    My <a href="https://www.youtube.com/channel/UCWVDjAcecHHa8UrEWMRGI8w" target="_blank">channel</a>. 
-    Tested by <a href="https://www.youtube.com/user/Heilerable" target="_blank">NlL5</a>. 
-    Updates on <a href="http://twitter.com/McHorsy" target="_blank">Twitter</a>.
-    <!-- Sorry if it looks like shit, but I'm not a designer -->
 </div>
